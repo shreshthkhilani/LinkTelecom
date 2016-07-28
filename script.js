95002c297b4c85dae8047529cfd97a93429feee9
Shreshth Khilani <shreshth.khilani@gmail.com>
2016-07-28 23:23:25 +0530

    about page and great scrolling experience

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	$('.itimg').hide();
+	$('.up').hide();
 	$('#itpanel').click(function () {
 		$('#tepanel').removeClass('activepanel');
 		$('#itpanel').addClass('activepanel');
@@ -13,21 +14,69 @@ $(document).ready(function () {
 		$('.teimg').show();
 	});
 	$(window).scroll(function () {
-    var height = $(window).scrollTop();
+		var height = $(window).scrollTop();
+		var boardht = $('#board').offset().top;
     if (height  > 0) {
       $('#header').addClass('headershadow');
     } else {
     	$('#header').removeClass('headershadow');
     }
+    if (height >= boardht) {
+    	$('.down').hide();
+    	$('.up').show();
+    } else {
+    	$('.down').show();
+    	$('.up').hide();
+    }
 	});
-	$('#downone').click(function () {
+	$('.down').click(function () {
+		onepagedown();
+	});
+	$('.up').click(function () {
+		scrolltothiselem('#top');
+	});
+	$(window).keydown(function (e) {
+		var key = e.keyCode;
+		if (key === 39 || key === 40) {
+			onepagedown();
+		} else if (key === 37 || key === 38) {
+			onepageup();
+		}
+	});
+
+	var onepagedown = function () {
+		var topht = $('#top').offset().top;
+		var aboutht = $('#about').offset().top;
+		var partnersht = $('#partners').offset().top;
+		var boardht = $('#board').offset().top;
+		var height = $(window).scrollTop();
+		if (height >= topht && height < aboutht) {
+			scrolltothiselem('#about');
+		} else if (height >= aboutht && height < partnersht) {
+			scrolltothiselem('#partners');
+		} else if (height >= partnersht && height < boardht) {
+			scrolltothiselem('#board');
+		}
+	}
+
+	var onepageup = function () {
+		var topht = $('#top').offset().top;
+		var aboutht = $('#about').offset().top;
+		var partnersht = $('#partners').offset().top;
+		var boardht = $('#board').offset().top;
+		var height = $(window).scrollTop();
+		if (height >= aboutht && height < partnersht) {
+			scrolltothiselem('#top');
+		} else if (height >= partnersht && height < boardht) {
+			scrolltothiselem('#about');
+		} else if (height >= boardht) {
+			scrolltothiselem('#partners');
+		}
+	}
+
+	var scrolltothiselem = function (elem) {
 		$('html, body').animate({
-        scrollTop: $("#partners").offset().top
+        scrollTop: $(elem).offset().top
     }, 1000);
-	});
-	$('#downtwo').click(function () {
-		$('html, body').animate({
-        scrollTop: $("#board").offset().top
-    }, 1000);
-	});
+	};
 });
