d553689f86077dbf217a19de22891a2d93269abf
Shreshth Khilani <shreshth.khilani@gmail.com>
2016-08-02 00:55:31 +0530

    landing

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('button#draft1').click(function () {
+		window.location = 'draft1/index.html';
+	});
+	$('button#draft2').click(function () {
+		window.location = 'draft2/index.html';
+	});
+	$('button#draft3').click(function () {
+		window.location = 'draft3/index.html';
+	});
+	$('button#draft3').click(function () {
+		window.location = 'draft3/index.html';
+	});
+});
