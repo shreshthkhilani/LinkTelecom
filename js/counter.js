47df3a272984bd11c7d6c1cc7ea41060e3c82f2c
Shreshth Khilani <shreshth.khilani@gmail.com>
2016-08-25 19:55:12 +0530

    colors

---
+++ js/counter.js
@@ -0,0 +1,14 @@
+$(window).load(function () {
+	$('.count').show();
+	$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+	});
+});
